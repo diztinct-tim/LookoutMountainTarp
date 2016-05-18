3c08176b1dad77488b9dfb84bb5ad12d40750625
Tim <tim@diztinct.com>
2016-05-18 17:16:13 -0400

    mobile homepage wip

--- assets/js/theme/home.js
+++ assets/js/theme/home.js
@@ -1,3 +1,19 @@
 import PageManager from '../page-manager';
+import $ from 'jquery';
 
-export default class Home extends PageManager {}
+export default class Home extends PageManager {
+
+	loaded(next) {
+
+		$(".home-featured-categories h4").click(function(){
+			$(this).toggleClass("open");
+			$(this).next("ul").slideToggle();
+		});
+
+	}
+
+
+
+
+
+ }
